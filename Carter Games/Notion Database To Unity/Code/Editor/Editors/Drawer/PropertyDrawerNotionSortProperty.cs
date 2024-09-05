4a6d495d9c2c9fd33de2e68017d5da3b085011b8
Jonathan <jonathan@carter.games>
2024-09-05 21:37:27 +0100

    :nut_and_bolt: 0.2.0 Pre-release upload
    - New version into pre-release for testing.
    - A load of new features and a lot of clean up

---
+++ Carter Games/Notion Database To Unity/Code/Editor/Editors/Drawer/PropertyDrawerNotionSortProperty.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CarterGames.Standalone.NotionData.Editor.Custom_Editors.Drawer
+{
+	[CustomPropertyDrawer(typeof(NotionSortProperty))]
+	public class PropertyDrawerNotionSortProperty : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+            
+			EditorGUI.BeginChangeCheck();
+
+			var indent = EditorGUI.indentLevel;
+			EditorGUI.indentLevel = 0;
+
+			var left = new Rect(position.x, position.y, position.width - 120f, EditorGUIUtility.singleLineHeight);
+			var right = new Rect(position.x + position.width - 17.5f, position.y, 120f, EditorGUIUtility.singleLineHeight);
+			
+			EditorGUI.PropertyField(left, property.Fpr("propertyName"), GUIContent.none);
+			EditorGUI.PropertyField(right, property.Fpr("ascending"), GUIContent.none);
+
+			if (EditorGUI.EndChangeCheck())
+			{
+				property.serializedObject.ApplyModifiedProperties();
+			}
+            
+			EditorGUI.indentLevel = indent;
+			EditorGUI.EndProperty();
+		}
+
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{
+			return EditorGUIUtility.singleLineHeight;
+		}
+	}
+}

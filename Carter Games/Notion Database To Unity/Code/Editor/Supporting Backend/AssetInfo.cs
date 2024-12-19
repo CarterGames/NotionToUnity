48dfd80b568355f1990b5632055ed992a29ac750
Jonathan <jonathan@carter.games>
2024-12-19 09:22:29 +0000

    🔩 Release prep
    - Releasing 0.4.0
    - Updated package json version number
    - Updated release date
    - Updated support url to be a remote changeable url.

--- Carter Games/Notion Database To Unity/Code/Editor/Supporting Backend/AssetInfo.cs
+++ Carter Games/Notion Database To Unity/Code/Editor/Supporting Backend/AssetInfo.cs
@@ -40,6 +40,6 @@ namespace CarterGames.Standalone.NotionData.Editor
         /// <remarks>
         /// Format is Y/M/D.
         /// </remarks>
-        public static string ReleaseDate => "2024/12/16";
+        public static string ReleaseDate => "2024/12/19";
     }
 }
